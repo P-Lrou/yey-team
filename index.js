b689173f67482373df0aff4e8388a43231bb97d9
P-Lrou <pl74rou@gmail.com>
2023-03-09 16:25:43 +0100

    edit style

---
+++ index.js
@@ -0,0 +1,7 @@
+function goPageTwoPartOne (){
+    document.querySelector(".firstPart").scrollIntoView({behavior: "smooth", block: "start"});
+}
+
+function goPageTwoPartTwo (){
+    document.querySelector(".secondPart").scrollIntoView({behavior: "smooth", block: "start"});
+}
