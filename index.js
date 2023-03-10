46c3de1457fddff745187be69bc9456415f2af18
P-Lrou <pl74rou@gmail.com>
2023-03-10 08:10:21 +0100

    start of second part

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function goPageTwoPartOne (){
-    document.querySelector(".firstPart").scrollIntoView({behavior: "smooth", block: "start"});
+function goPageTwoPartOne() {
+  document
+    .querySelector(".firstPart")
+    .scrollIntoView({ behavior: "smooth", block: "start" });
 }
 
-function goPageTwoPartTwo (){
-    document.querySelector(".secondPart").scrollIntoView({behavior: "smooth", block: "start"});
+function goPageTwoPartTwo() {
+  document
+    .querySelector(".secondPart")
+    .scrollIntoView({ behavior: "smooth", block: "start" });
 }
