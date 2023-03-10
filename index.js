840bc51ecec7c94cb2802e66197198cfa5065271
P-Lrou <pl74rou@gmail.com>
2023-03-10 12:00:10 +0100

    remove F12

--- index.js
+++ index.js
@@ -60,3 +60,22 @@ document.addEventListener("DOMContentLoaded", () => {
 setTimeout(() => {
   document.querySelector('#button').style.animation = "float 4s ease-in-out infinite"
 }, 4000);
+
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
